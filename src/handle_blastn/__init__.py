d0b6d40c7f568800ad28b5aa5d6d03e2243d7613
Johannes <johannes-programming@posteo.org>
2023-11-13 00:42:56 +0100

    Version 0.0.2

--- src/handle_blastn/__init__.py
+++ src/handle_blastn/__init__.py
@@ -1,5 +1,2 @@
-def main(args=None):
-    print("Hello World!")
-    
-if __name__ == '__main__':
-    main() 
+from ._clines import *
+from ._summaries import *
--- src/handle_blastn/__main__.py
+++
@@ -1,4 +0,0 @@
-from handle_blastn import main
-
-if __name__ == '__main__':
-    main()
